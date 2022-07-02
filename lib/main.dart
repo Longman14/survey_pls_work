1d53c947e3ff30e3e90551e746739c2f476e4382
Longman14 <hassandamilola580@gmail.com>
2022-07-02 01:00:09 +0100

    created the first question interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:survey_app_new/questionspage.dart';
+import 'package:survey_app_new/welcomepage.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -9,65 +9,3 @@ void main() {
   ));
 }
 
-class WelcomePage extends StatelessWidget {
-      @override
-  Widget build(BuildContext context){
-        return Scaffold(
-          body: Container(
-            color: Colors.blueAccent,
-            child: Stack(
-
-              children: [
-                Positioned.fill(child:
-                Image.asset('assets/imgs/healthbk.jpg', fit: BoxFit.cover),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Center(
-                      child: ClipOval(
-                        child: Container(
-                          width: 150,
-                          height: 150,
-                          color: Colors.blue,
-                          alignment: Alignment.center,
-                          child: Image.asset('assets/health.png'),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text('World Health Survey', textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold,
-                    ),),
-                    SizedBox(height: 30),
-                    Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: FlatButton(
-                        onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder:
-                              (context) => QuestionsPage() ),);
-                        },
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
-                        ),
-                        color: Colors.white,
-                        padding: EdgeInsets.all(25),
-                        child: Text('Take Survey', style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold
-                        ),),
-                      ),
-                    )
-                  ],
-                  ),
-                )
-              ]
-            )
-          )
-        );
-      }
-}
--- lib/questionspage.dart
+++ lib/questionspage.dart
@@ -1,13 +1,122 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 class QuestionsPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        actions: [
+SizedBox(height: 10),
+        ],
+      ),
+      backgroundColor: Colors.blue[600],
       body: Center(
-      child: Text('yada yada')
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Container(
+            width: double.infinity,
+            height: 130.0,
+            margin: EdgeInsets.only(bottom: 10.0, left: 30.0,right: 30.0),
+            padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Text(
+                'This is where questions go',
+                    textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+              color: null,
+                border: Border.all(color: Colors.lightBlueAccent),
+                borderRadius: BorderRadius.circular(20.0),
+            ),
+                child: Center(
+                  child: Text('This is for the answers', style: TextStyle(
+                    fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.w500,
+                  ),),
+                ),
+
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: null,
+                border: Border.all(color: Colors.lightBlueAccent),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Center(
+                child: Text('This is for the answers', style: TextStyle(
+                  fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.w500,
+                ),),
+              ),
+
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: null,
+                border: Border.all(color: Colors.lightBlueAccent),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Center(
+                child: Text('This is for the answers', style: TextStyle(
+                  fontSize: 15.0, color: Colors.white, fontWeight: FontWeight.w500,
+                ),),
+              ),
+
+            ),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 40.0),
+                padding: EdgeInsets.all(25),
+              ),
+              onPressed: (){},
+              child: Text(
+            'Next Question'
+          ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              '0/20', style: TextStyle(
+              fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.white,
+            ),
+            ),
+          )
+        ],
+      )
     )
     );
   }
